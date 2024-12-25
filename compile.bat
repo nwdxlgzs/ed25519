61294ecdf59a8c00e603bf49287df0160d863182
难忘的旋律 <47502008+nwdxlgzs@users.noreply.github.com>
2024-12-25 19:10:44 +0800

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,5 @@
 @echo off
-..\LLVM-19.1.6\bin\clang.exe -Wno-deprecated-declarations -O3 main.c src/add_scalar.c src/fe.c src/ge.c src/key_exchange.c src/keypair.c src/sc.c src/seed.c src/sha512.c src/sign.c src/verify.c -o ed25519.exe -ladvapi32
+@REM ..\LLVM-19.1.6\bin\clang.exe -Wno-deprecated-declarations -O3 main.c src/add_scalar.c src/fe.c src/ge.c src/key_exchange.c src/keypair.c src/sc.c src/seed.c src/sha512.c src/sign.c src/verify.c -o ed25519.exe -ladvapi32
+..\LLVM-19.1.6\bin\clang.exe -shared -o ed25519.dll src/add_scalar.c src/fe.c src/ge.c src/key_exchange.c src/keypair.c src/sc.c src/seed.c src/sha512.c src/sign.c src/verify.c -ladvapi32 -Wl,/def:ed25519.def
+..\LLVM-19.1.6\bin\clang.exe -Wno-deprecated-declarations -O3 main.c -o ed25519.exe -led25519
 timeout /t 5
